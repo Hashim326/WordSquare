7639d3c94a156b0e9eb6cb555e471d1cd6631a55
Hashim326 <hash1bash@gmail.com>
2026-06-08 22:08:11 +0100

    Updated how the dictionary file is loaded in during production run

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -8,7 +9,15 @@ public class Dictionary {
     public HashSet<String> words = new HashSet<>();
 
     public Dictionary(int wordLength, HashMap<Character, Integer> charCount) {
-        try (BufferedReader br = new BufferedReader(new FileReader("dictionary.txt"))) {
+
+        InputStream is = Main.class.getClassLoader()
+                                   .getResourceAsStream("dictionary.txt");
+
+        if (is == null) {
+            throw new RuntimeException("File not found!");
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.length() == wordLength) {
